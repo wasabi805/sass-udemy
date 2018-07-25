cb8cf15756405c4fb65be279836dedee452503e9
‘Tim <timothy.j.ocampo@gmail.com>
2018-07-25 16:49:19 -0700

    initial : udemy-sass-lesson

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class App extends Component{
+    render(){
+        return(<div>
+            <p>Another One</p>
+        </div>)
+    }
+}
+
+export default App
---
+++ webpack.config.js
@@ -0,0 +1,47 @@
+
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports={
+
+    entry: './src/index.js',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+
+    module:{
+        rules:[
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use:{
+                    loader: 'babel-loader'
+                }
+            },
+
+            {
+                test: /\.css$/,
+                use:['style-loader', 'css-loader']
+            },
+
+            {
+                test: /\.scss$/,
+                use:[
+                    {loader: "style-loader"},
+                    {loader: "css-loader"},
+                    {loader: "sass-loader",
+                        options:{}
+                    }
+                ]
+            }
+
+        ]
+    },
+
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+};
